759a0e1241e29338041a41d4b308f08a8ac472fa
Maksim Kraev <maxim.kraev@gmail.com>
2017-04-21 21:54:09 +0100

    some rain logic

--- src/rain/rain.cpp
+++ src/rain/rain.cpp
@@ -2,13 +2,31 @@
 
 // Checks for RAIN_CHECK_INTERVAL,
 
-cCallbackBool *_stateChangeCallback;
+static cCallbackBool *_stateChangeCallback;
+static TimedAction *rainCheckTimer;
+
+static bool isCurrentlyRain = false;
 
 void rainSensorSetup(TCallbackBool &stateChangeCallback) {
   _stateChangeCallback = &stateChangeCallback;
 
 }
 
-void rainSensorCheck() {
+static bool getIsRain() {
+  return false; // no sensor yet
+}
 
+static void onCheck() {
+  static bool rain = getIsRain();
+  if (rain && !isCurrentlyRain) {
+    isCurrentlyRain = true;
+    _stateChangeCallback->Execute(true);
+  } else if (!rain && isCurrentlyRain) {
+    isCurrentlyRain = true;
+    _stateChangeCallback->Execute(false);
+  }
+}
+
+void rainSensorCheck() {
+  rainCheckTimer = new TimedAction(4000, onCheck);
 }
